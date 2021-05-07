c584cefc76ff4efe79b4463e6b614946cd87f9bb
Ana Clara Cruz <anaclaralcruz@poli.ufrj.br>
2021-05-06 21:14:56 -0300

    Add class Grafo and its constructor

--- Grafo.cpp
+++ Grafo.cpp
@@ -8,11 +8,54 @@
 /* "Grafo.cpp" ------------------------------------------------- */
 /* Contem os metodos e atributos da classe Grafo */
 
-using namespace std ;
+#include "Grafo.h"
 
-class Estadual {
-    public:
+Grafo::Grafo (string arquivo){
+    vector <string> vetorDePalavras = separarPalavras (arquivo) ;
+    for (long usigned int indice = 0 ; indice < vetorDePalavras.size() ; indice++){
+        Vertice vertice (vetorDePalavras[indice]);
+        vertices.push_back(vertice);
+    }
+}
 
-    private:
+// Separa as palavras de um arquivo e retorna um Vetor de palavras
+vector <string> Grafo::separarPalavras(string nomeDoArquivo){
 
-};
+  vector <string> paragrafos , palavrasSeparadas;
+  string palavra ;
+  long unsigned int indice1, indice2 ;
+
+  paragrafos = readLines (nomeDoArquivo);
+
+  for (indice1 = 0 ; indice1 < paragrafos.size() ; indice1++) {
+    string paragrafo = paragrafos[indice1];
+
+    for(indice2=0 ; indice2 < paragrafo.length() ; indice2++) {
+      if(paragrafo[indice2] != ' '){
+        palavra.push_back(paragrafo[indice2]);
+        if (indice2 == paragrafo.length() -1)
+          palavrasSeparadas.push_back(palavra);
+      }
+      else {
+        palavrasSeparadas.push_back(palavra);
+        palavra = "";
+      }
+    }
+  }
+  return palavrasSeparadas;
+}
+
+//Le um arquivo e devolve os paragrafos como strings separadas em um vetor 
+vector <string> Grafo::readLines (string arquivo){
+
+  vector <string> vetor;
+  string linha;
+
+  ifstream file(arquivo);
+  
+  while (getline(file, linha)) {
+    vetor.push_back(linha);
+  }
+  return vetor;
+}
+
--- Grafo.h
+++ Grafo.h
@@ -8,3 +8,27 @@
 /* "Grafo.h" ------------------------------------------------- */
 /* Inicializa a classe Grafo */
 
+#include <string>
+
+#include "vertice.h"
+
+using namespace std; 
+
+class Grafo {
+    public:
+        // Construtor - recebe um arquivo
+        Grafo (string);
+
+    private:
+        Vector <Vertice> vertices;
+
+        Vertice criarVertice(string);
+
+        // Read lines from a file (recebe um arquivo)
+        vector <string> readLines (string);
+
+        // Separa as palavras de um arquivo (recebe um arquivo e devolve uma lista de palavras)
+        vector <string> separarPalavras(string);
+
+
+};
